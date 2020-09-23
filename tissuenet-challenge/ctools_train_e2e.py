970e0ad4a4806df58f303ecae72829fb039e8056
Waliens <r.mormont@uliege.be>
2020-09-23 16:06:26 +0200

    add clustertools script for training

---
+++ tissuenet-challenge/ctools_train_e2e.py
@@ -0,0 +1,58 @@
+import os
+from clustertools import ParameterSet, ConstrainedParameterSet, Experiment, set_stdout_logging, CTParser
+
+from cli_computation import CliComputationFactory
+from e2e_classifier_train import main
+
+
+def env_parser():
+    parser = CTParser()
+    parser.add_argument("--device", dest="device", default="cuda:0")
+    parser.add_argument("--n_jobs", dest="n_jobs", default=1, type=int)
+    return parser
+
+
+def cstrnt_batchsize_zoom(**kwargs):
+    return (kwargs["zoom_level"] == 2 and kwargs["batch_size"] == 32) \
+            or (kwargs["zoom_level"] == 1 and kwargs["batch_size"] == 16) \
+            or (kwargs["zoom_level"] == 0 and kwargs["batch_size"] == 8)
+
+
+if __name__ == "__main__":
+
+    set_stdout_logging()
+    # Define the parameter set: the domain each variable can take
+
+    environment, namespace = env_parser().parse()
+    env_params = dict(namespace._get_kwargs())
+
+    data_path = "/scratch/users/rmormont/tissuenet"
+    env = {"image_path": os.path.join(data_path, "patches"),
+           "metadata_path": os.path.join(data_path, "metadata"),
+           "model_path": os.path.join(data_path, "models"),
+           "device": namespace.device,
+           "n_jobs": namespace.n_jobs
+           }
+
+    os.makedirs(env["image_path"], exist_ok=True)
+    os.makedirs(env["metadata_path"], exist_ok=True)
+    os.makedirs(env["model_path"], exist_ok=True)
+
+    param_set = ParameterSet()
+    param_set.add_parameters(pretrained=["imagenet"])
+    param_set.add_parameters(architecture=["densenet121"])
+    param_set.add_parameters(epochs=5)
+    param_set.add_parameters(batch_size=[32])
+    param_set.add_parameters(zoom_level=[2])
+    param_set.add_parameters(train_size=0.7)
+    param_set.add_parameters(random_seed=42)
+    param_set.add_parameters(learning_rate=0.001)
+
+    constrained = ConstrainedParameterSet(param_set)
+    constrained.add_constraints(bsize_zoom=cstrnt_batchsize_zoom)
+
+    # Wrap it together as an experiment
+    experiment = Experiment("tissuenet-e2e-train", constrained, CliComputationFactory(main, **env))
+
+    # Finally run the experiment
+    environment.run(experiment)
