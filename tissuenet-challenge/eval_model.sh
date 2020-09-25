26f332a580dd501b60ae4967a7c4aa87ba29a2cf
Waliens <r.mormont@uliege.be>
2020-09-25 21:49:24 +0200

    move to 'all' partition

--- tissuenet-challenge/eval_model.sh
+++ tissuenet-challenge/eval_model.sh
@@ -2,9 +2,8 @@
 #
 #SBATCH --job-name=eval_model
 #SBATCH --output=eval_model.log
-#SBATCH --partition=debug
 #SBATCH --cpus-per-task=5
 #SBATCH --gres=gpu:1
-#SBATCH --time=10:00
+#SBATCH --time=16:00:00
 
 /home/rmormont/miniconda3/envs/py-gpu/bin/python eval_model_on_slides.py --device "cuda:0" --n_jobs 5 -m "$SCRATCH/tissuenet/metadata" -i "$SCRATCH/tissuenet/wsis"
