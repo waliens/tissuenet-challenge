f73c99c20075b7293c29a0da6d4effa8361e0c41
Waliens <r.mormont@uliege.be>
2020-10-01 18:17:11 +0200

    include drawing code

--- tissuenet-challenge/analyse_classif_by_hand.py
+++ tissuenet-challenge/analyse_classif_by_hand.py
@@ -1,13 +1,21 @@
 import csv
+import os
+import pickle
+
 import numpy as np
 from collections import defaultdict
 
+from PIL import Image, ImageDraw
 from numpy.random.mtrand import RandomState
 from shapely.geometry import box
+from shapely.affinity import affine_transform
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, confusion_matrix, make_scorer, roc_auc_score
 from sklearn.model_selection import train_test_split, GridSearchCV, KFold
+from skimage.color import hsv2rgb
+import pyvips
 
+from assets.inference import determine_tissue_extract_level
 from svm_classifier_train import compute_challenge_score, group_per_slide
 
 
@@ -39,7 +47,7 @@ def get_dataset_cls_dict(slides, cls_dict, slide2cls):
     for i, slide in enumerate(slides):
         x[i] = np.array([cls_dict[slide][cls] for cls in range(4)])
         y[i] = slide2cls[slide]
-    x = x / np.sum(x, axis=1, keepdims=True)
+    # x = x / np.sum(x, axis=1, keepdims=True)
     return x, y
 
 
@@ -60,6 +68,48 @@ def print_eval(y_true, y_pred):
     print("> slide sco: ", val_slide_score)
     print("> slide cm : ")
     print(val_slide_cm)
+
+
+def get_color_by_scale(val, minval=0, maxval=1):
+    s, v = 0.45, 1.0
+    min_h, max_h = 0, 0.305556
+    prop = (maxval - val) / (maxval - minval)
+    h = min_h + (max_h - min_h) * prop
+    return tuple((hsv2rgb([h, s, v]) * 255).astype(np.uint8))
+
+
+def get_color_by_class(cls):
+    return [
+        (180, 255, 140),
+        (255, 234, 140),
+        (255, 192, 140),
+        (255, 148, 148)
+    ][cls]
+
+
+def draw_pred_on_slide(slide_path, preds, value_fn, color_fn, opacity=127, size=2048, base_zl=2):
+    level = determine_tissue_extract_level(slide_path, desired_processing_size=size)
+    slide = pyvips.Image.new_from_file(slide_path, page=level)
+
+    black = (0, 0, 0, 0)
+    mask = Image.new('RGBA', (slide.width, slide.height), black)
+    draw = ImageDraw.Draw(mask)
+    zoom_ratio = 2 ** (base_zl - level)
+    t_matrix = [zoom_ratio, 0, 0, zoom_ratio, 0, 0]
+
+    for rect, probas in preds:
+        color = color_fn(value_fn(probas))
+        with_opacity = color + (opacity, )
+        draw.rectangle(affine_transform(rect, t_matrix).bounds, fill=with_opacity, outline=black)
+
+    np_image = np.ndarray(
+        buffer=slide.write_to_memory(),
+        dtype=np.uint8,
+        shape=[slide.height, slide.width, slide.bands]
+    )
+    pil_slide = Image.fromarray(np_image).convert("RGBA")
+    img = Image.alpha_composite(pil_slide, mask)
+    return pil_slide, img.convert("RGB")
 
 
 def main():
@@ -88,6 +138,43 @@ def main():
     print("best_score:", grid.best_score_)
     print_eval(y_test, grid.best_estimator_.predict(x_test))
 
+    rf.set_params(**grid.best_params_)
+    rf.fit(np.vstack([x_train, x_test]), np.hstack([y_train, y_test]))
+
+    with open("random_forest.pkl", "wb+") as file:
+        pickle.dump(rf, file)
+
+    # base_path = "/scratch/users/rmormont/tissuenet/"
+    # wsi_path = os.path.join(base_path, "wsis")
+    # # entropy_path = os.path.join(base_path, "entropy")
+    # pred_path = os.path.join(base_path, "pred")
+    # os.makedirs(pred_path, exist_ok=True)
+    # opacity = 150
+    # for slidename in slidenames:
+    #     print(slidename, "pred")
+    #     pil_slide, pred_draw = draw_pred_on_slide(
+    #         slide_path=os.path.join(wsi_path, slidename),
+    #         preds=per_filename[slidename],
+    #         opacity=opacity,
+    #         value_fn=lambda p: np.argmax(p),
+    #         color_fn=get_color_by_class,
+    #         size=4096
+    #     )
+    #     name = slidename.split(".", 1)[0]
+    #     pred_draw.save(os.path.join(pred_path, name + "_pred.png"))
+    #     pil_slide.save(os.path.join(pred_path, name + ".png"))
+    #
+    #     print(slidename, "entro")
+    #     _, entropy_draw = draw_pred_on_slide(
+    #         slide_path=os.path.join(wsi_path, slidename),
+    #         preds=per_filename[slidename],
+    #         opacity=opacity,
+    #         value_fn=lambda p: (-np.sum(p * np.log(p))),
+    #         color_fn=lambda v: get_color_by_scale(v, minval=0, maxval=-np.log(0.25)),
+    #         size=4096
+    #     )
+    #     name = slidename.split(".", 1)[0]
+    #     entropy_draw.save(os.path.join(pred_path, name + "_entro.png"))
 
 if __name__ == "__main__":
     main()
