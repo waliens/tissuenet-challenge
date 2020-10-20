baf7bcc587fa417f5a7011db34ce500548bb0a19
Romain Mormont <romainmormont@hotmail.com>
2020-10-20 14:56:30 +0200

    remove ensembling + add smart parameters listing

--- tissuenet-challenge/analyse_e2e_classif_benchmark.py
+++ tissuenet-challenge/analyse_e2e_classif_benchmark.py
@@ -11,44 +11,58 @@ from sklearn.model_selection import train_test_split, KFold, GridSearchCV
 from analyse_classif_by_hand import get_results, get_dataset_probas_dict, get_dataset_cls_dict, print_eval
 from svm_classifier_train import group_per_slide, compute_challenge_score
 
-MODEL_TO_LR = {
-    "densenet121_imagenet_e_16_val_0.8092_sco_0.9523_z1_1602269610.556128.pth": 0.0001,
-    "densenet121_imagenet_e_37_val_0.7714_sco_0.9280_z1_1602297064.95862.pth": 0.001,
-    "densenet121_mtdp_e_35_val_0.8125_sco_0.9548_z1_1602294993.20278.pth": 0.0001,
-    "densenet121_mtdp_e_42_val_0.7812_sco_0.9372_z1_1602302274.124153.pth": 0.001,
-    "resnet18_imagenet_e_20_val_0.7936_sco_0.9400_z1_1602284618.815257.pth": 0.0001,
-    "resnet18_imagenet_e_25_val_0.7656_sco_0.9342_z1_1602288221.466545.pth": 0.001,
-    "resnet34_imagenet_e_29_val_0.8018_sco_0.9463_z1_1602292926.445457.pth": 0.0001,
-    "resnet34_imagenet_e_39_val_0.7664_sco_0.9310_z1_1602304468.116915.pth": 0.001,
-    "resnet50_imagenet_e_13_val_0.8092_sco_0.9463_z1_1602278212.460756.pth": 0.0001,
-    "resnet50_imagenet_e_31_val_0.7640_sco_0.9329_z1_1602294758.970913.pth": 0.001,
-    "resnet50_mtdp_e_13_val_0.8084_sco_0.9457_z1_1602278600.525486.pth": 0.0001,
-    "resnet50_mtdp_e_22_val_0.7590_sco_0.9336_z1_1602288070.805371.pth": 0.001,
-    "densenet121_imagenet_e_56_val_0.8067_sco_0.9482_z2_1602274272.266287.pth": 0.0001,
-    "densenet121_imagenet_e_18_val_0.7985_sco_0.9426_z2_1602270033.686283.pth": 0.001,
-    "densenet121_mtdp_e_13_val_0.8035_sco_0.9437_z2_1602261301.097603.pth": 0.0001,
-    "densenet121_mtdp_e_20_val_0.8166_sco_0.9529_z2_1602270971.797485.pth": 0.001,
-    "resnet18_imagenet_e_29_val_0.7829_sco_0.9394_z2_1602273845.577956.pth": 0.0001,
-    "resnet18_imagenet_e_20_val_0.7689_sco_0.9356_z2_1602271013.427048.pth": 0.001,
-    "resnet34_imagenet_e_13_val_0.7928_sco_0.9416_z2_1602268977.799415.pth": 0.0001,
-    "resnet34_imagenet_e_23_val_0.7697_sco_0.9355_z2_1602272360.440732.pth": 0.001,
-    "resnet50_imagenet_e_16_val_0.7919_sco_0.9454_z2_1602270120.230495.pth": 0.0001,
-    "resnet50_imagenet_e_31_val_0.7582_sco_0.9326_z2_1602274314.81669.pth": 0.001,
-    "resnet50_mtdp_e_42_val_0.7977_sco_0.9470_z2_1602278632.321392.pth": 0.0001,
-    "resnet50_mtdp_e_27_val_0.7673_sco_0.9352_z2_1602274284.932415.pth": 0.001,
-    "densenet121_imagenet_e_27_val_0.7714_sco_0.9405_z3_1602497438.170938.pth": 0.0001,
-    "densenet121_imagenet_e_42_val_0.7903_sco_0.9427_z3_1602500515.90902.pth": 0.001,
-    "densenet121_mtdp_e_29_val_0.7821_sco_0.9394_z3_1602497634.902743.pth": 0.0001,
-    "densenet121_mtdp_e_29_val_0.7870_sco_0.9433_z3_1602497749.006002.pth": 0.001,
-    "resnet18_imagenet_e_52_val_0.7599_sco_0.9376_z3_1602503546.608902.pth": 0.0001,
-    "resnet18_imagenet_e_30_val_0.7401_sco_0.9293_z3_1602499053.351917.pth": 0.001,
-    "resnet34_imagenet_e_49_val_0.7656_sco_0.9394_z3_1602502754.449239.pth": 0.0001,
-    "resnet34_imagenet_e_16_val_0.7426_sco_0.9309_z3_1602496152.337871.pth": 0.001,
-    "resnet50_imagenet_e_59_val_0.7821_sco_0.9447_z3_1602505790.992334.pth": 0.0001,
-    "resnet50_imagenet_e_29_val_0.7393_sco_0.9275_z3_1602498166.004176.pth": 0.001,
-    "resnet50_mtdp_e_16_val_0.7771_sco_0.9360_z3_1602495632.231674.pth": 0.0001,
-    "resnet50_mtdp_e_22_val_0.7475_sco_0.9287_z3_1602496806.118761.pth": 0.001
-}
+# 2nd
+# MODEL_TO_LR = {
+#     "densenet121_imagenet_e_16_val_0.8092_sco_0.9523_z1_1602269610.556128.pth": 0.0001,
+#     "densenet121_imagenet_e_37_val_0.7714_sco_0.9280_z1_1602297064.95862.pth": 0.001,
+#     "densenet121_mtdp_e_35_val_0.8125_sco_0.9548_z1_1602294993.20278.pth": 0.0001,
+#     "densenet121_mtdp_e_42_val_0.7812_sco_0.9372_z1_1602302274.124153.pth": 0.001,
+#     "resnet18_imagenet_e_20_val_0.7936_sco_0.9400_z1_1602284618.815257.pth": 0.0001,
+#     "resnet18_imagenet_e_25_val_0.7656_sco_0.9342_z1_1602288221.466545.pth": 0.001,
+#     "resnet34_imagenet_e_29_val_0.8018_sco_0.9463_z1_1602292926.445457.pth": 0.0001,
+#     "resnet34_imagenet_e_39_val_0.7664_sco_0.9310_z1_1602304468.116915.pth": 0.001,
+#     "resnet50_imagenet_e_13_val_0.8092_sco_0.9463_z1_1602278212.460756.pth": 0.0001,
+#     "resnet50_imagenet_e_31_val_0.7640_sco_0.9329_z1_1602294758.970913.pth": 0.001,
+#     "resnet50_mtdp_e_13_val_0.8084_sco_0.9457_z1_1602278600.525486.pth": 0.0001,
+#     "resnet50_mtdp_e_22_val_0.7590_sco_0.9336_z1_1602288070.805371.pth": 0.001,
+#     "densenet121_imagenet_e_56_val_0.8067_sco_0.9482_z2_1602274272.266287.pth": 0.0001,
+#     "densenet121_imagenet_e_18_val_0.7985_sco_0.9426_z2_1602270033.686283.pth": 0.001,
+#     "densenet121_mtdp_e_13_val_0.8035_sco_0.9437_z2_1602261301.097603.pth": 0.0001,
+#     "densenet121_mtdp_e_20_val_0.8166_sco_0.9529_z2_1602270971.797485.pth": 0.001,
+#     "resnet18_imagenet_e_29_val_0.7829_sco_0.9394_z2_1602273845.577956.pth": 0.0001,
+#     "resnet18_imagenet_e_20_val_0.7689_sco_0.9356_z2_1602271013.427048.pth": 0.001,
+#     "resnet34_imagenet_e_13_val_0.7928_sco_0.9416_z2_1602268977.799415.pth": 0.0001,
+#     "resnet34_imagenet_e_23_val_0.7697_sco_0.9355_z2_1602272360.440732.pth": 0.001,
+#     "resnet50_imagenet_e_16_val_0.7919_sco_0.9454_z2_1602270120.230495.pth": 0.0001,
+#     "resnet50_imagenet_e_31_val_0.7582_sco_0.9326_z2_1602274314.81669.pth": 0.001,
+#     "resnet50_mtdp_e_42_val_0.7977_sco_0.9470_z2_1602278632.321392.pth": 0.0001,
+#     "resnet50_mtdp_e_27_val_0.7673_sco_0.9352_z2_1602274284.932415.pth": 0.001,
+#     "densenet121_imagenet_e_27_val_0.7714_sco_0.9405_z3_1602497438.170938.pth": 0.0001,
+#     "densenet121_imagenet_e_42_val_0.7903_sco_0.9427_z3_1602500515.90902.pth": 0.001,
+#     "densenet121_mtdp_e_29_val_0.7821_sco_0.9394_z3_1602497634.902743.pth": 0.0001,
+#     "densenet121_mtdp_e_29_val_0.7870_sco_0.9433_z3_1602497749.006002.pth": 0.001,
+#     "resnet18_imagenet_e_52_val_0.7599_sco_0.9376_z3_1602503546.608902.pth": 0.0001,
+#     "resnet18_imagenet_e_30_val_0.7401_sco_0.9293_z3_1602499053.351917.pth": 0.001,
+#     "resnet34_imagenet_e_49_val_0.7656_sco_0.9394_z3_1602502754.449239.pth": 0.0001,
+#     "resnet34_imagenet_e_16_val_0.7426_sco_0.9309_z3_1602496152.337871.pth": 0.001,
+#     "resnet50_imagenet_e_59_val_0.7821_sco_0.9447_z3_1602505790.992334.pth": 0.0001,
+#     "resnet50_imagenet_e_29_val_0.7393_sco_0.9275_z3_1602498166.004176.pth": 0.001,
+#     "resnet50_mtdp_e_16_val_0.7771_sco_0.9360_z3_1602495632.231674.pth": 0.0001,
+#     "resnet50_mtdp_e_22_val_0.7475_sco_0.9287_z3_1602496806.118761.pth": 0.001
+# }
+
+
+def build_model_dict(experiment):
+    datacube = build_datacube(experiment)
+    model_dict = dict()
+    base_params = ["architecture", "pretrained"]
+    aug_params = list(set(datacube.parameters).difference(base_params))
+    for _, cube in datacube.iter_dimensions(*base_params):
+        for aug_pvalues, aug_cube in cube.iter_dimensions(*aug_params):
+            if aug_cube.diagnose()["Missing ratio"] > 0.0:
+                continue
+            model_dict[aug_cube("models")[-1]] = [aug_cube.metadata[param] for param in aug_params]
+    return aug_params, model_dict
 
 
 def cls_dict_aggr(cls_dict, tile_pred):
@@ -70,13 +84,16 @@ OUT_CLS_DICT = "cls_dict"
 OUT_PROBAS = "probas"
 
 
-def extract_tiles_individual(datacube):
+def extract_tiles_individual(datacube, train_exp_name):
     per_method = dict()
+    aug_params, model_to_params = build_model_dict(train_exp_name)
+    print(aug_params)
     for (model_name, ), model_cube in datacube.iter_dimensions("model_filename"):
-        for param_tuple, subcube in model_cube.iter_dimensions("batch_size", "architecture", "zoom_level", "tile_size"):
+        for param_tuple, subcube in model_cube.iter_dimensions(*model_cube.parameters):
             if subcube.diagnose()["Missing ratio"] > 0.0:
                 continue
-            _, arch, zoom, _ = param_tuple
+            arch = subcube.metadata["architecture"]
+            zoom = subcube.metadata["zoom_level"]
             splitted_name = model_name.rsplit(".", 1)[0].split("_")
             pretrained = splitted_name[1]
 
@@ -84,12 +101,12 @@ def extract_tiles_individual(datacube):
             tile_dict = {(filename, tile): probas[i]
                           for i, (filename, tile)
                           in enumerate(zip(subcube("filenames"), subcube("tiles")))}
-            per_method[(arch, pretrained, zoom, str(MODEL_TO_LR[model_name]))] = tile_dict
+            per_method[(arch, pretrained, zoom) + tuple(str(v) for v in model_to_params[model_name])] = tile_dict
     return per_method
 
 
-def extract_predictor_slide(datacube, tile_pred_aggr_fn=None):
-    per_method_tiles = extract_tiles_individual(datacube)
+def extract_predictor_slide(datacube, train_exp_name, tile_pred_aggr_fn=None):
+    per_method_tiles = extract_tiles_individual(datacube, train_exp_name)
     per_method_merged = dict()
     for method, tile_dict in per_method_tiles.items():
         slide_dict = dict()
@@ -99,10 +116,10 @@ def extract_predictor_slide(datacube, tile_pred_aggr_fn=None):
     return per_method_merged
 
 
-def extract_ensemble_predictor_slide(datacube, to_merge, proba_aggr_fn=None, tile_pred_aggr_fn=None):
+def extract_ensemble_predictor_slide(datacube, to_merge, train_exp_name, proba_aggr_fn=None, tile_pred_aggr_fn=None):
     if proba_aggr_fn is None:
         proba_aggr_fn = lambda f, s: f + s
-    per_method_tiles = extract_tiles_individual(datacube)
+    per_method_tiles = extract_tiles_individual(datacube, train_exp_name)
     to_merge = set(to_merge)
     param_order = {k: i for i, k in enumerate(["architecture", "pretrained", "zoom_level", "lr"])}
     per_method_merged = dict()
@@ -125,16 +142,17 @@ def extract_ensemble_predictor_slide(datacube, to_merge, proba_aggr_fn=None, til
 
 
 def main():
-    datacube = build_datacube("tissuenet-e2e-eval-2nd")
+    datacube = build_datacube("tissuenet-e2e-eval-3rd")
 
     print(datacube.metrics)
     print(datacube.metadata)
     print(datacube.domain)
 
-    per_method_cls_dict = extract_predictor_slide(datacube, cls_dict_aggr)
-    per_method_probas = extract_predictor_slide(datacube, probas_sum_aggr)
-    per_method_cls_dict_forest = extract_ensemble_predictor_slide(datacube, to_merge=["architecture", "pretrained", "lr"], tile_pred_aggr_fn=cls_dict_aggr)
-    per_method_probas_forest = extract_ensemble_predictor_slide(datacube, to_merge=["architecture", "pretrained", "lr"], tile_pred_aggr_fn=probas_sum_aggr)
+    train_exp_name = "tissuenet-e2e-train-3rd"
+    per_method_cls_dict = extract_predictor_slide(datacube, train_exp_name, cls_dict_aggr)
+    per_method_probas = extract_predictor_slide(datacube, train_exp_name, probas_sum_aggr)
+    # per_method_cls_dict_forest = extract_ensemble_predictor_slide(datacube, to_merge=["architecture", "pretrained", "lr"], tile_pred_aggr_fn=cls_dict_aggr)
+    # per_method_probas_forest = extract_ensemble_predictor_slide(datacube, to_merge=["architecture", "pretrained", "lr"], tile_pred_aggr_fn=probas_sum_aggr)
 
     print(per_method_probas.keys())
     print(per_method_cls_dict.keys())
@@ -155,8 +173,8 @@ def main():
     for aggr_method, per_method, prepare_func in [
         ("cls_dict", per_method_cls_dict, get_dataset_cls_dict),
         ("probas", per_method_probas, get_dataset_probas_dict),
-        ("cls_dict_ensemble", per_method_cls_dict_forest, get_dataset_cls_dict),
-        ("probas_ensemble", per_method_probas_forest, get_dataset_probas_dict)
+        # ("cls_dict_ensemble", per_method_cls_dict_forest, get_dataset_cls_dict),
+        # ("probas_ensemble", per_method_probas_forest, get_dataset_probas_dict)
     ]:
         for method, x in per_method.items():
             x_train, y_train = prepare_func(train_slides, x, slide2cls, norm=False)
