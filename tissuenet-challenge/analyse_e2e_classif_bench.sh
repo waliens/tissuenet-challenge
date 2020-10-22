c38db503a33feba41b6d27cdc50df0e5c0239f6a
Romain Mormont <romainmormont@hotmail.com>
2020-10-22 16:23:19 +0200

    rewrite benchmark analysis

--- tissuenet-challenge/analyse_e2e_classif_bench.sh
+++ tissuenet-challenge/analyse_e2e_classif_bench.sh
@@ -2,8 +2,8 @@
 #
 #SBATCH --job-name=analyse_e2e_classif_benchmark
 #SBATCH --output=analyse_e2e_classif_benchmark.log
-#SBATCH --mem=24G
+#SBATCH --mem=16G
 #SBATCH --cpus-per-task=8
-#SBATCH --time=32:00:00
+#SBATCH --time=20:00:00
 
 /home/rmormont/miniconda3/envs/py-gpu/bin/python analyse_e2e_classif_benchmark.py
