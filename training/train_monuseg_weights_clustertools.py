469eff8bbb68b3babc16153eb6c8f8132d378dc5
Waliens <r.mormont@uliege.be>
2021-12-20 13:36:27 +0100

    add last experiments

--- training/train_monuseg_weights_clustertools.py
+++ training/train_monuseg_weights_clustertools.py
@@ -108,8 +108,8 @@ if __name__ == "__main__":
     param_set.add_parameters(monu_ms=[65682867])
     param_set.add_separator()
     param_set.add_parameters(monu_ms=[91090292])
-    # param_set.add_separator()
-    # param_set.add_parameters(monu_ms=[93410762, 96319575])
+    param_set.add_separator()
+    param_set.add_parameters(monu_ms=[93410762, 96319575])
 
     constrained = ConstrainedParameterSet(param_set)
     constrained.add_constraints(weight_exclude=weight_exclude)
